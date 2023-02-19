04a846483ac71fb3f8b11b16aa89a4098197cba5
Kristifor Milchev <kristifor@collaborativesoft.com>
2023-02-19 14:09:26 +0200

    Adding theme options

--- lib/src/entity/calendar_config.dart
+++ lib/src/entity/calendar_config.dart
@@ -1,9 +1,11 @@
+import 'package:event_schedule_calendar/src/entity/day_cell_config.dart';
 import 'package:event_schedule_calendar/src/entity/palette_config.dart';
 import 'package:event_schedule_calendar/src/entity/weekday_label_config.dart';
 
 class CalendarConfig {
   WeekdayLabelConfig? weekdayLabelConfig;
   PaletteConfig? paletteConfig;
+  CellConfig? dayCellConfig;
 
   final int beginWeekday;
   final String remainEventCountPattern;
@@ -11,6 +13,7 @@ class CalendarConfig {
   CalendarConfig({
     this.weekdayLabelConfig,
     this.paletteConfig,
+    this.dayCellConfig,
     this.beginWeekday = DateTime.sunday,
     this.remainEventCountPattern = '他 %s 件',
   }) : assert(DateTime.monday <= beginWeekday &&
@@ -18,8 +21,13 @@ class CalendarConfig {
     if (weekdayLabelConfig == null) {
       this.weekdayLabelConfig = WeekdayLabelConfig();
     }
+
     if (paletteConfig == null) {
       this.paletteConfig = PaletteConfig();
+    }
+
+    if (dayCellConfig == null) {
+      this.dayCellConfig = CellConfig();
     }
   }
 }
--- lib/src/entity/palette_config.dart
+++ lib/src/entity/palette_config.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 class PaletteConfig {
   final Color today;
+  final Color boxColor;
+  final Color todayFontColor;
+  final Color selectedBorder;
+  final Color dayFontColorInMonth;
+  final Color dayFontColorNotInMonth;
 
   PaletteConfig({
     this.today = Colors.blue,
+    this.boxColor = Colors.black,
+    this.todayFontColor = Colors.white,
+    this.selectedBorder = Colors.transparent,
+    this.dayFontColorInMonth = Colors.black,
+    this.dayFontColorNotInMonth = Colors.black45,
   });
 }
